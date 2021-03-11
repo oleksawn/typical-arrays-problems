4644df10ada4c1c3cb2297a91698b4802b38cef7
Oleksandra <oleksatu@gmail.com>
2021-03-11 18:07:00 +0200

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  else{
+    return array.reduce(function(min, el){return (el < min)? el : min});
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  else{
+    return array.reduce(function(max, el){return (el > max)? el : max});
+  }
 }
+exports.max([1,20,3,4,8]);
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  else{
+      return array.reduce(function(sum, el){return sum + el;}) / array.length;
+  }
 }
